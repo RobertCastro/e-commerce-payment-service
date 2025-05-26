2cabe00479a4e695c0c40eb122cff042d29bb09d
Robert Castro <oneroberth@gmail.com>
2025-05-26 14:06:01 -0500

    docs(config): Add .env.example with environment variable documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        WOMPI_PUBLIC_KEY: Joi.string().required(),
+        WOMPI_PRIVATE_KEY: Joi.string().required(),
+        WOMPI_EVENTS_KEY: Joi.string().required(),
+        WOMPI_INTEGRITY_KEY: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
