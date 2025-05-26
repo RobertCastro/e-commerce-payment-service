128f2a95692db797c4d8b3c09b536281d778cd2c
Robert Castro <oneroberth@gmail.com>
2025-05-26 18:30:55 -0500

    build: Add class-validator and class-transformer, enable global ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+  await app.listen(port);
+  console.log(`Aplicación corriendo en: ${await app.getUrl()}`);
 }
 bootstrap();
