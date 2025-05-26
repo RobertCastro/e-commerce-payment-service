f949add52c593df518850c27246212fe95c803d4
Robert Castro <oneroberth@gmail.com>
2025-05-26 15:32:25 -0500

    feat(infra): Configure postgres connection using typeorm and ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from 'joi';
 
 import { AppController } from './app.controller';
@@ -21,6 +22,22 @@ import { AppService } from './app.service';
         DB_USERNAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
         DB_NAME: Joi.string().required(),
+      }),
+    }),
+    // <-- Configuración de TypeORM
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/modules/**/*.entity{.ts,.js}'],
+        synchronize: true, // Lo agrego solo mientras se desarrolla
+        logging: true, // Mientras se desarrolla
       }),
     }),
   ],
