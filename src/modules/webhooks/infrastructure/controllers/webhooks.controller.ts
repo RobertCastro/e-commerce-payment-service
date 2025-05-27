2673da2a1180a36081918b64ba26306ad6cf7ac9
Robert Castro <oneroberth@gmail.com>
2025-05-27 16:55:31 -0500

    feat(webhooks): implement Wompi event handling with DTO and use case

--- src/modules/webhooks/infrastructure/controllers/webhooks.controller.ts
+++ src/modules/webhooks/infrastructure/controllers/webhooks.controller.ts
@@ -1,22 +1,29 @@
-import { Controller, Post, Body, Logger, Headers } from '@nestjs/common';
-// import { Request } from 'express';
+import { Controller, Post, Body, Logger, HttpException, HttpStatus } from '@nestjs/common';
+import { HandleWompiEventUseCase } from '../../application/use-cases/handle-wompi-event.use-case';
+import { WompiEventDto } from '../../application/dto/wompi-event.dto';
 
 @Controller('webhooks')
 export class WebhooksController {
   private readonly logger = new Logger(WebhooksController.name);
 
+  constructor(private readonly handleEventUseCase: HandleWompiEventUseCase) {}
+
   @Post('wompi')
-  async handleWompiWebhook(
-    @Body() payload: any,
-    @Headers() headers: any,
-    // @Req() req: RawBodyRequest<Request>,
-  ) {
-    this.logger.log('Webhook de Wompi recibido!');
-    this.logger.log('Headers:', JSON.stringify(headers, null, 2));
-    this.logger.log('Payload:', JSON.stringify(payload, null, 2));
+  async handleWompiWebhook(@Body() payload: WompiEventDto) {
+    this.logger.log('Payload de Webhook de Wompi recibido:', JSON.stringify(payload, null, 2));
 
-    // TODO verificar la firma
+    if (!payload || !payload.data || !payload.data.transaction) {
+      this.logger.error('Estructura de payload de webhook de Wompi inválida.');
+      throw new HttpException('Payload inválido', HttpStatus.BAD_REQUEST);
+    }
 
-    return { received: true };
+    const result = await this.handleEventUseCase.execute(payload);
+
+    if (result.ok) {
+      return { received: true };
+    } else {
+      this.logger.error(`Error al procesar el webhook: ${result.error.message}`);
+      throw new HttpException('Error al procesar el webhook', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
--- src/modules/webhooks/webhooks.module.ts
+++ src/modules/webhooks/webhooks.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { WebhooksController } from './infrastructure/controllers/webhooks.controller';
 import { CheckoutModule } from '../checkout/checkout.module';
+import { ProductsModule } from '../products/products.module';
+import { HandleWompiEventUseCase } from './application/use-cases/handle-wompi-event.use-case';
 
 @Module({
-  imports: [CheckoutModule],
+  imports: [CheckoutModule, ProductsModule],
   controllers: [WebhooksController],
-  providers: [
-    // TODO Guard y UseCase
-  ],
+  providers: [HandleWompiEventUseCase],
 })
 export class WebhooksModule {}
