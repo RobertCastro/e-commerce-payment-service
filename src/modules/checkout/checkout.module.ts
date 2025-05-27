0c4a6441a6b1c32fb4beec4551040c00aede53af
Robert Castro <oneroberth@gmail.com>
2025-05-27 10:56:27 -0500

    feat(checkout): Update WompiAdapter  (token, signature)

--- src/modules/checkout/checkout.module.ts
+++ src/modules/checkout/checkout.module.ts
@@ -1,21 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpModule } from '@nestjs/axios';
 import { Transaction } from './domain/transaction.entity';
 import { Customer } from './domain/customer.entity';
 import { Delivery } from './domain/delivery.entity';
 import { ITransactionRepository } from './domain/ports/transaction.repository.port';
 import { PostgresTransactionRepository } from './infrastructure/persistence/postgres-transaction.repository';
+import { IPaymentGateway } from './domain/ports/payment.gateway.port';
+import { WompiPaymentGatewayAdapter } from './infrastructure/payment-gateways/wompi.adapter';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction, Customer, Delivery])],
+  imports: [TypeOrmModule.forFeature([Transaction, Customer, Delivery]), HttpModule],
   controllers: [],
   providers: [
     PostgresTransactionRepository,
+    WompiPaymentGatewayAdapter,
     {
       provide: ITransactionRepository,
       useClass: PostgresTransactionRepository,
     },
   ],
-  exports: [ITransactionRepository],
+  exports: [ITransactionRepository, IPaymentGateway],
 })
 export class CheckoutModule {}
