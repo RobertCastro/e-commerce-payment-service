ef4a6e56bf6868b7c75d40bafccf22dc706ad84c
Robert Castro <oneroberth@gmail.com>
2025-05-27 21:49:44 -0500

    feat(product): enhance ProductResponseDto with Swagger API properties and update ProductsController with API operation and response metadata

--- src/modules/products/infrastructure/controllers/products.controller.ts
+++ src/modules/products/infrastructure/controllers/products.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, HttpException, HttpStatus, Logger } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { ListAvailableProductsUseCase } from '../../application/use-cases/list-available-products.use-case';
 import { ProductResponseDto } from '../../application/dto/product.response.dto';
 
+@ApiTags('Products')
 @Controller('products')
 export class ProductsController {
   private readonly logger = new Logger(ProductsController.name);
@@ -9,6 +11,16 @@ export class ProductsController {
   constructor(private readonly listProductsUseCase: ListAvailableProductsUseCase) {}
 
   @Get()
+  @ApiOperation({ summary: 'Listar todos los productos disponibles' })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de productos obtenida exitosamente.',
+    type: [ProductResponseDto],
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Error interno del servidor al intentar listar los productos.',
+  })
   async findAll(): Promise<ProductResponseDto[]> {
     this.logger.log('Recibido request para listar productos disponibles');
 
